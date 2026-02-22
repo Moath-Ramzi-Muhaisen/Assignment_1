80959c60dc1a0903b5336e8c3754bdfec5f940c9
MoathRamzi <moathramzi18@gmail.com>
2026-02-22 21:04:14 +0300

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please Enter The Invoice");
+var invoice = Convert.ToInt32(Console.ReadLine());
+
+double discount = 0;
+double finalAmount = invoice;
+
+if (invoice >= 500)
+{
+    discount = invoice * 0.20;
+    finalAmount = invoice - discount;
+}
+else if (invoice >= 300 && invoice < 500)
+{
+    discount = invoice * 0.10;
+    finalAmount = invoice - discount;
+}
+
+Console.WriteLine("Final Amount: " + finalAmount);
